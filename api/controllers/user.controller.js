edf915d5fda5c37021bd5f87e29a07c4fc309dcb
AshwinKumarRk <rameshkumar.a@northeastern.edu>
2021-10-04 16:47:27 -0400

    Add GET (Non-Authenticated)

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -2,6 +2,25 @@ const db = require("../models");
 const User = db.users;
 const {v4: uuid} = require('uuid');
 
+//Retrieve a user with unique id
+exports.findOne = (req, res) => {
+    const id = req.params.id
+
+    User.findOne({where: {id: id}})
+    .then(users => {
+        let userData = {
+            id: users.id,
+            username: users.username,
+            firstName: users.firstName,
+            lastName: users.lastName,
+            account_created: users.createdAt,
+            account_updated: users.updateAt
+        }
+        res.status(200).send(userData)
+    })
+
+}
+
 //Create a user with a unique id
 exports.create = (req, res) => {
     const uid = uuid()
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -4,5 +4,6 @@ const users = require('../controllers/user.controller')
 
 //Route to navigate to requests
 router.post('/', users.create)
+router.get('/:id', users.findOne)
 
 module.exports = router
