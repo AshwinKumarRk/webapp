3d3f31c8411eaa4de040ff63c379986d468321e0
AshwinKumarRk <rameshkumar.a@northeastern.edu>
2021-11-28 15:09:16 -0500

    Add Read Replica Access

--- api/config.js
+++ api/config.js
@@ -6,6 +6,9 @@ module.exports = {
     USER: process.env.DB_USER,
     PASSWORD: process.env.DB_PASS,
     DB: process.env.DB_NAME,
+    RR_HOST: process.env.DB_RR_HOST,
+    RR_USER: process.env.DB_RR_USER,
+    RR_PASSWORD: process.env.DB_RR_PASS,
     dialect: "mysql",
   };
   
